21ffdfc5a955fbf7fa0920932b64accbb5187cc3
ItaloBonfim <nsouzaitalo@hotmail.com>
2020-11-15 23:48:43 -0300

    autenticacao, autorizacao, tokens, ss, profile

--- src/main/java/com/LeagueSocial/Services/Profile/AccountProfileService.java
+++ src/main/java/com/LeagueSocial/Services/Profile/AccountProfileService.java
@@ -4,17 +4,21 @@ package com.LeagueSocial.Services.Profile;
  * Interface padrão de implementação
 */
 
+import com.LeagueSocial.DTO.AccountDTO;
 import com.LeagueSocial.Domain.Account;
 import com.LeagueSocial.Domain.Publication;
 import org.springframework.data.domain.Page;
 
+import java.util.Optional;
 import java.util.function.Consumer;
 
-public interface AccountProfileService {
+public interface AccountClassPerfil {
 
     public Account SelectDate(Integer id);
 
-    public Account InsertData(Account obj);
+    public Account FindUserByEmail(String email);
+
+    public Account InsertData(AccountDTO obj);
 
     public void DeleteDate(Integer id);
 
